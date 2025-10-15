908054ea4af2486dac5cc578144e5c3b4d0c4f30
Neel Patel <91706742+NotNeelPatel@users.noreply.github.com>
2025-10-14 22:21:50 -0400

    format code, add project, fix css

--- assets/dropdown.js
+++ assets/dropdown.js
@@ -1,32 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const faqItems = document.querySelectorAll('.dropdown-item');
+document.addEventListener("DOMContentLoaded", () => {
+    const jobs = document.querySelectorAll(".dropdown-item");
 
-    faqItems.forEach(item => {
-        item.addEventListener('click', () => {
-            const answer = item.querySelector(".dropdown-content");
+    jobs.forEach((item) => {
+        item.addEventListener("click", () => {
+            const details = item.querySelector(".dropdown-content");
             const arrow = item.querySelector(".dropdown-arrow");
             item.classList.toggle("active");
-            if(item.classList.contains('active')) {
-                answer.style.maxHeight = answer.scrollHeight + "px";;
+            if (item.classList.contains("active")) {
+                details.style.maxHeight = details.scrollHeight + "px";
                 arrow.style.transform = "rotate(-135deg)";
+                jobs.forEach((otherItem) => {
+                    if (otherItem !== item) {
+                        otherItem.classList.remove("active");
+                        const otherDetails =
+                            otherItem.querySelector(".dropdown-content");
+                        const otherArrow =
+                            otherItem.querySelector(".dropdown-arrow");
+                        otherDetails.style.maxHeight = 0;
+                        otherArrow.style.transform = "rotate(0deg)";
+                    }
+                });
             } else {
-                answer.style.maxHeight = 0
+                details.style.maxHeight = 0;
                 arrow.style.transform = "rotate(0deg)";
-            }        
+            }
         });
     });
 
-    const hamburger = document.getElementById('hamburger');
+    const hamburger = document.getElementById("hamburger");
 
-    hamburger.addEventListener('click', () => {
-        const experience = document.querySelector('.experience');
+    hamburger.addEventListener("click", () => {
+        const experience = document.querySelector(".experience");
         if (experience.style.visibility === "visible") {
-            experience.style.transform = "translateX(-100%)";            hamburger.textContent = "☰"
-            experience.style.visibility = "hidden"
+            experience.style.transform = "translateX(-100%)";
+            hamburger.textContent = "☰";
+            experience.style.visibility = "hidden";
             return;
         }
         experience.style.transform = "translateX(0)";
-        experience.style.visibility = "visible"
-        hamburger.textContent = "✕"
+        experience.style.visibility = "visible";
+        hamburger.textContent = "✕";
     });
 });
